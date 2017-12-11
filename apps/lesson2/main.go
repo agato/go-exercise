8dce94ca599d379af1920834c1b40b0bfb80a51e
田口 雅教 <taguchi_masanori@cyberagent.co.jp>
2017-12-11 19:16:29 +0900

    lession2追加

--- apps/lesson2/main.go
+++ apps/lesson2/main.go
@@ -7,6 +7,8 @@ import (
 	"sync"
 	"text/template"
 	"os"
+	"agato/go-exercise/apps/lesson2/trace"
+	"flag"
 )
 
 type templateHandler struct {
@@ -19,15 +21,31 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	t.templ.Execute(w, data)
 }
 
 func main() {
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	port := os.Getenv("PORT")
+
+
+	flag.Parse() // フラグを解釈します
+
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
 	if port == "" {
 		log.Fatal("PORT environment variable was not set")
 	}
+
+	http.Handle("/room", r)
+
+	go r.run()
+
+	log.Println("Webサーバーを開始します。ポート: ", port)
+
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal("Could not listen: ", err)
